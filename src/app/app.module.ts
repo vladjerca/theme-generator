751ba682a1ca7924d5a0ea60d726ce67464d000a
Vlad Jerca <vlad.jerca@gmail.com>
2019-08-07 03:51:41 +0300

    feat(theme-generator): define application and status sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { PaletteGeneratorModule } from './components/palette-generator/palette-generator.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    PaletteGeneratorModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
