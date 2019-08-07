368fbc11e6d84efa3ca0880dd95da09009708c6a
Vlad Jerca <vlad.jerca@gmail.com>
2019-08-07 12:11:57 +0300

    feat(palette-generator): implement contrast color and display a11y info

--- src/app/models/palette-color.ts
+++ src/app/models/palette-color.ts
@@ -1,6 +1,9 @@
 import {
     tinycolor,
     TinyColor,
+    mostReadable,
+    isReadable,
+    readability,
 } from '@ctrl/tinycolor';
 
 export type PaletteIntensity = '900'
@@ -18,19 +21,70 @@ export type PaletteIntensity = '900'
     | 'A400'
     | 'A700';
 
-export abstract class PaletteColor {
+export class Color {
+    private static WHITE = new Color('#FFF');
+    private static BLACK = new Color('#000');
+
     protected _color: TinyColor;
+    protected _contrast: Color;
+
+    private _readabilityScore: string;
+    private _info: string;
+
+    public isReadable(level: 'AA' | 'AAA') {
+        return isReadable(this._color, this.contrast._color, {
+            level,
+        });
+    }
 
     constructor(
-        hex: string,
-        public readonly intensity?: PaletteIntensity,
+        hex: string
     ) {
         this._color = tinycolor(hex);
     }
 
-    public abstract get palette();
+    public get contrast() {
+        return this._contrast = this._contrast ||
+            new Color(
+                mostReadable(
+                    this._color,
+                    [Color.WHITE.hex, Color.BLACK.hex],
+                    {
+                        level: 'AAA',
+                        includeFallbackColors: true,
+                        size: 'small',
+                    },
+                ).toHexString()
+            );
+    }
+
+    public get readabilityScore() {
+        return this._readabilityScore =
+            this._readabilityScore ||
+            readability(this._color, this.contrast._color).toFixed(2);
+    }
 
     public get hex() {
         return this._color.toHexString();
     }
+
+    public get info() {
+        return this._info = this._info || `
+            BG: ${this.hex}
+            FG: ${this.contrast.hex}
+            AA: ${this.readabilityScore} ${this.isReadable('AA') ? '>= 4.5 ✔' : '< 4.5 ❌'}
+            AAA: ${this.readabilityScore} ${this.isReadable('AAA') ? '>= 7 ✔' : '< 7 ❌'}
+        `;
+    }
+}
+
+export abstract class PaletteColor extends Color {
+    constructor(
+        hex: string,
+        public readonly intensity?: PaletteIntensity,
+    ) {
+        super(hex);
+    }
+
+    public abstract get palette();
 }
